b3af4488b69832f577dd4869888c14ceb9808bbe
Olivier LOYNET <olivierloynet@gmail.com>
2026-01-26 11:06:03 +0100

    v1.23 - main menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -2054,7 +2054,7 @@ function setupMenuObserver() {
 
 window.onload = init;
 if ( 'serviceWorker' in navigator ) {
-    navigator.serviceWorker.register( 'service-worker.js?v1.22' )
+    navigator.serviceWorker.register( 'service-worker.js?v1.23' )
         .then( ( reg )  => console.log( 'Service Worker enregistré', reg ) )
         .catch( ( err ) => console.log( 'Erreur Service Worker',     err ) );
 }
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'v1.22';
+const CACHE_NAME = 'v1.23';
 
 const STATIC_ASSETS = [
     './',
