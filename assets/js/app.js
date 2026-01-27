0fadb4c5c5fd89f91b6433a3879faeed79a30d3c
Olivier LOYNET <olivierloynet@gmail.com>
2026-01-27 08:21:47 +0100

    v1.32

--- assets/js/app.js
+++ assets/js/app.js
@@ -2191,7 +2191,7 @@ function setupMenuObserver() {
 
 window.onload = init;
 if ( 'serviceWorker' in navigator ) {
-    navigator.serviceWorker.register( 'service-worker.js?v1.31' )
+    navigator.serviceWorker.register( 'service-worker.js?v1.32' )
         .then( ( reg )  => console.log( 'Service Worker enregistré', reg ) )
         .catch( ( err ) => console.log( 'Erreur Service Worker',     err ) );
 }
