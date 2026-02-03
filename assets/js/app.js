ad40288e5adc8284ec1a5529902273f964042046
Olivier LOYNET <olivierloynet@gmail.com>
2026-02-03 16:39:29 +0100

    v1.65

--- assets/js/app.js
+++ assets/js/app.js
@@ -138,8 +138,8 @@ async function init() {
         AppState.currentLang          = urlParams.get( 'lang' ) || 'fr';
         document.documentElement.lang = AppState.currentLang;
 
-        const configFile = 'config/config.json?v1.64';
-        const langConfigFile = AppState.currentLang === 'en' ? 'config/config_en.json?v1.64' : 'config/config_fr.json?v1.64';
+        const configFile = 'config/config.json?v1.65';
+        const langConfigFile = AppState.currentLang === 'en' ? 'config/config_en.json?v1.65' : 'config/config_fr.json?v1.65';
 
         // 1. & 2. Charger les configs en parallèle
         const [mainConfigResponse, langConfigResponse] = await Promise.all([
@@ -2784,7 +2784,7 @@ function setupMenuObserver() {
 
 window.onload = init;
 if ( 'serviceWorker' in navigator ) {
-    navigator.serviceWorker.register( 'service-worker.js?v1.64' )
+    navigator.serviceWorker.register( 'service-worker.js?v1.65' )
         .then( ( reg )  => console.log( 'Service Worker enregistré', reg ) )
         .catch( ( err ) => console.log( 'Erreur Service Worker',     err ) );
 }
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'v1.64';
+const CACHE_NAME = 'v1.65';
 
 const STATIC_ASSETS = [
     './',
