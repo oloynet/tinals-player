09926ca1213a8e9b72e8194aecc76b079f39f4b3
Olivier LOYNET <olivierloynet@gmail.com>
2026-02-10 13:44:00 +0100

    adjusts for socials pills

--- assets/js/app.js
+++ assets/js/app.js
@@ -141,8 +141,8 @@ async function init() {
         AppState.currentLang          = urlParams.get( 'lang' ) || 'fr';
         document.documentElement.lang = AppState.currentLang;
 
-        const configFile = 'config/config.json?v1.88';
-        const langConfigFile = AppState.currentLang === 'en' ? 'config/config_en.json?v1.88' : 'config/config_fr.json?v1.88';
+        const configFile = 'config/config.json?v1.89';
+        const langConfigFile = AppState.currentLang === 'en' ? 'config/config_en.json?v1.89' : 'config/config_fr.json?v1.89';
 
         // 1. & 2. Charger les configs en parallèle
         const [mainConfigResponse, langConfigResponse] = await Promise.all([
@@ -2997,7 +2997,7 @@ function setupMenuObserver() {
 
 window.onload = init;
 if ( 'serviceWorker' in navigator ) {
-    navigator.serviceWorker.register( 'service-worker.js?v1.88' )
+    navigator.serviceWorker.register( 'service-worker.js?v1.89' )
         .then( ( reg )  => console.log( 'Service Worker enregistré', reg ) )
         .catch( ( err ) => console.log( 'Erreur Service Worker',     err ) );
 }
